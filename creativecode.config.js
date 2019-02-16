6c78b40fc4a3c9863f5cebf661c540d75fc121ad
Stefterv <stef@steftervelde.nl>
2019-02-16 17:33:05 +0100

    pm2 settings

---
+++ creativecode.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'Creative Code Berlin',
+    script: 'nuxt',
+
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+};
