b3b3c681320f442e9b3f478a75b20402bca09c0e
Stefterv <stef@steftervelde.nl>
2020-03-14 12:35:33 +0100

    Update covid19.vue

--- pages/covid19.vue
+++ pages/covid19.vue
@@ -1,31 +1,41 @@
 <template>
   <article>
-    <pre>
-Hello everyone,
+    <aside>
+      Berlin, March 14th, 2020
+    </aside>
+    Hello everyone,<br /><br />Co.Up—the usual venue for our meet-ups—has
+    announced they will be temporarily closing the space at least through April
+    13th. We support their decision and we will not be looking for an
+    alternative venue and we are suspending our meet-ups until further notice.
+    We are exploring online alternatives. We will keep you posted via our usual
+    channels (see further down).<br /><br />The advice of Berlin Public Medical
+    Officer is to limit all social contact, and even though events of our size
+    are not officially banned, we believe this is the only responsible course of
+    action to help slow down the spread of the virus, lessen the strain on our
+    healthcare system, and protect vulnerable members of our community at
+    large.<br /><br />We will post about eventual online events, and let you
+    know when physical events resume on our Twitter account, Mattermost chat,
+    and Newsletter.<br /><br />Stay safe, stay strong, wash your hands, call
+    your loved ones ❤️<br />– The Creative Code Berlin Team<br /><br />Read
+    Co.Up's official announcement here:<br />
+    <a href="https://co-up.de/2020/03/12/coup-temporarily-closed/"
+      >https://co-up.de/2020/03/12/coup-temporarily-closed/</a
+    ><br /><br />
 
-Co.Up—the usual venue for our meet-ups—has announced they will be temporarily closing the space at least through April 13th. We support their decision and we will not be looking for an alternative venue and we are suspending our meet-ups until further notice. We are exploring online alternatives. We will keep you posted via our usual channels (see further down).
-
-The advice of Berlin Public Medical Officer is to limit all social contact, and even though events of our size are not officially banned, we believe this is the only responsible course of action to help slow down the spread of the virus, lessen the strain on our healthcare system, and protect vulnerable members of our community at large.
-
-We will post about eventual online events, and let you know when physical events resume on our Twitter account, Mattermost chat, and Newsletter.
-
-Stay safe, stay strong, wash your hands, call your loved ones ❤️
-– The Creative Code Berlin Team
-
-Read Co.Up's official announcement here: 
-<a href="https://co-up.de/2020/03/12/coup-temporarily-closed/">https://co-up.de/2020/03/12/coup-temporarily-closed/</a>
-
-Follow this link for official updates from the German Health Ministry:
-<a href="https://www.bundesgesundheitsministerium.de/en/press/2020/coronavirus.html">https://www.bundesgesundheitsministerium.de/en/press/2020/coronavirus.html</a>
-    </pre>
+    Follow this link for official updates from the German Health Ministry:<br />
+    <a
+      href="https://www.bundesgesundheitsministerium.de/en/press/2020/coronavirus.html"
+      >https://www.bundesgesundheitsministerium.de/en/press/2020/coronavirus.html</a
+    >
   </article>
 </template>
 
 <style lang="scss" scoped>
+aside {
+  font-weight: normal;
+}
 article {
   padding: 80px 40px;
-}
-pre {
-  white-space: pre-line;
+  word-spacing: 0;
 }
 </style>
