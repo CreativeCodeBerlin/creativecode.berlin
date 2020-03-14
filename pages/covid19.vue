a46e2e423684b6ecacf57c0eb3f30b3b203c1fde
Stefterv <stef@steftervelde.nl>
2020-03-14 12:27:31 +0100

    Create covid19.vue

---
+++ pages/covid19.vue
@@ -0,0 +1,31 @@
+<template>
+  <article>
+    <pre>
+Hello everyone,
+
+Co.Up—the usual venue for our meet-ups—has announced they will be temporarily closing the space at least through April 13th. We support their decision and we will not be looking for an alternative venue and we are suspending our meet-ups until further notice. We are exploring online alternatives. We will keep you posted via our usual channels (see further down).
+
+The advice of Berlin Public Medical Officer is to limit all social contact, and even though events of our size are not officially banned, we believe this is the only responsible course of action to help slow down the spread of the virus, lessen the strain on our healthcare system, and protect vulnerable members of our community at large.
+
+We will post about eventual online events, and let you know when physical events resume on our Twitter account, Mattermost chat, and Newsletter.
+
+Stay safe, stay strong, wash your hands, call your loved ones ❤️
+– The Creative Code Berlin Team
+
+Read Co.Up's official announcement here: 
+<a href="https://co-up.de/2020/03/12/coup-temporarily-closed/">https://co-up.de/2020/03/12/coup-temporarily-closed/</a>
+
+Follow this link for official updates from the German Health Ministry:
+<a href="https://www.bundesgesundheitsministerium.de/en/press/2020/coronavirus.html">https://www.bundesgesundheitsministerium.de/en/press/2020/coronavirus.html</a>
+    </pre>
+  </article>
+</template>
+
+<style lang="scss" scoped>
+article {
+  padding: 80px 40px;
+}
+pre {
+  white-space: pre-line;
+}
+</style>
