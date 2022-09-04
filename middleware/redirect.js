14b315ca017e128409593406506cb1d9fe083110
Stef Tervelde <stef@steftervelde.nl>
2022-09-04 16:16:32 +0200

    redirect fix

--- middleware/redirect.js
+++ middleware/redirect.js
@@ -2,6 +2,6 @@ import redirects from "../assets/redirect";
 
 export default function(ctx) {
   redirects.forEach(el => {
-    if (ctx.route.fullPath == el.path) ctx.redirect(301, el.target);
+    if (ctx.route.fullPath.startsWith(el.path)) ctx.redirect(301, el.target);
   });
 }
