ac3db3ec884766851148312be6a9e2911f57d994
Stef Tervelde <stef@steftervelde.nl>
2022-11-02 23:23:14 +0100

    chat link fix

--- assets/redirect.js
+++ assets/redirect.js
@@ -2,51 +2,51 @@ export default [
   {
     title: "Meetup",
     path: "/meetup",
-    target: "https://www.meetup.com/creativecodeberlin/"
+    target: "https://www.meetup.com/creativecodeberlin/",
   },
   {
     title: "Stammtisch Pad",
     path: "/ccs",
-    target: "https://funprogramming.org/pad/p/ccs"
+    target: "https://funprogramming.org/pad/p/ccs",
   },
   {
     title: "Stammtisch Archive",
     path: "/archive",
-    target: "https://creativecodeberlin.github.io/Stammtisch/"
+    target: "https://creativecodeberlin.github.io/Stammtisch/",
   },
   {
     title: "Jam Pad",
     path: "/jam",
-    target: "https://funprogramming.org/pad/p/ccjam"
+    target: "https://funprogramming.org/pad/p/ccjam",
   },
   {
     title: "Twitter",
     path: "/twitter",
-    target: "https://twitter.com/CreativeCodeBLN"
+    target: "https://twitter.com/CreativeCodeBLN",
   },
   {
     title: "Chat",
     path: "/chat",
-    target: "http://chat.creativecode.berlin"
+    target: "https://funprogramming.org/chat",
   },
   {
     title: "Discord",
     path: "/discord",
-    target: "https://discord.gg/EnqmrU9"
+    target: "https://discord.gg/EnqmrU9",
   },
   {
     title: "Telegram",
     path: "/telegram",
-    target: "https://t.me/creativecodeberlin"
+    target: "https://t.me/creativecodeberlin",
   },
   {
     title: "Instagram",
     path: "/instagram",
-    target: "https://www.instagram.com/creativecodebln/"
+    target: "https://www.instagram.com/creativecodebln/",
   },
   {
     title: "Github",
     path: "/github",
-    target: "https://github.com/CreativeCodeBerlin"
-  }
+    target: "https://github.com/CreativeCodeBerlin",
+  },
 ];
