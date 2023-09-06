05abf4588bb315ed07a3728a929ec9bc23c8e142
Stef Tervelde <stef@steftervelde.nl>
2023-09-06 21:42:06 +0200

    redirect rules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,22 @@
+import fs from 'node:fs'
+import redirects from "./assets/redirects";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxtjs/google-fonts"],
   googleFonts: {
     families: {
-      Comfortaa: true
+      Comfortaa: true,
     },
   },
+  hooks: {
+    "build:done"() {
+        const file = redirects.map((redirect) => {
+          return `${redirect.path} /${redirect.target}/ 200`;
+        }).join("\n");
+
+        fs.writeFileSync("public/_redirects",file);
+      },
+    }
 });
