e30757f354535ac7484cb372e30bf4e4765bd2f8
Stef Tervelde <stef@steftervelde.nl>
2024-04-05 20:06:30 +0200

    grievance policy fixes #60

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,17 @@
 <template>
-    <section class="links">
-      <h2>Links</h2>
-      <ul>
-        <template v-for="redirect in redirects" :key="redirect.path">
-          <a :href="redirect.target">
-            <li class="redirect">
-              <main>{{ redirect.title }}</main>
-              <aside>{{ redirect.path }} </aside>
-            </li>
-          </a>
-        </template>
-      </ul>
-    </section>
+  <section class="links">
+    <h2>Links</h2>
+    <ul>
+      <template v-for="redirect in redirects" :key="redirect.path">
+        <a :href="redirect.target">
+          <li class="redirect">
+            <main>{{ redirect.title }}</main>
+            <aside>{{ redirect.path }} </aside>
+          </li>
+        </a>
+      </template>
+    </ul>
+  </section>
 </template>
 
 <script setup>
@@ -19,24 +19,28 @@ import redirects from 'assets/redirects';
 </script>
 
 <style lang="scss" scoped>
-.links{
+.links {
   padding: var(--pad2);
-  .redirect{
+
+  .redirect {
     background: var(--primary);
     margin: var(--pad) 0;
     padding: var(--pad) var(--pad2);
     color: white;
     border-radius: 2px;
     transition: 0.2s ease-in-out;
-    aside{
+
+    aside {
       font-size: 0.8em;
       opacity: 0.5;
       padding-left: var(--pad);
     }
+
     &:hover {
       transform: translateX(10px);
       mix-blend-mode: difference;
-      .white &{
+
+      .white & {
         filter: invert(1);
       }
     }
