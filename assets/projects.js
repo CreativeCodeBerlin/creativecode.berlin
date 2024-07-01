7d63b82cc7ecbf0848da82d2bd266b5033037977
Stef Tervelde <stef@steftervelde.nl>
2024-07-01 08:37:20 +0200

    use embed instead of view

--- assets/projects.js
+++ assets/projects.js
@@ -63,10 +63,10 @@ export default [
     "scale": "1"
   },
   {
-  "url": "https://www.shadertoy.com/view/lX3XWl",
-  "author": "Matthias Meissen",
-  "author_url": "https://www.instagram.com/matthiasmeissen/",
-  "color": "black",
-  "scale": "1"
-}
+    "url": "https://www.shadertoy.com/embed/lX3XWl?gui=false&t=0&paused=true&muted=false",
+    "author": "Matthias Meissen",
+    "author_url": "https://www.instagram.com/matthiasmeissen/",
+    "color": "black",
+    "scale": "1"
+  }
 ];
